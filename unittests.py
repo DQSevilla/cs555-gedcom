8c7dcc5098b9dc0aaa8ff7ccb5e8b79419e45475
Mengqiao2020 <lmq199507@hotmail.com>
2020-10-06 05:50:31 +0800

    Update unittests.py

--- unittests.py
+++ unittests.py
@@ -186,6 +186,17 @@ class US12TestCase(unittest.TestCase):
             examples.exampleFamilyWithWidow
         ))
 
+# Testing US35 & US36
+class TestRecent30DayBornorDeath(unittest.TestCase):
+    def test_verifyBirthAtRecent30Days(self):
+        self.assertTrue(CS555.verifyBirthAtRecent30Days(examples.examplePersonRecentBirth))
+
+    def test_verifyDeathAtRecent30Days(self):
+        self.assertTrue(CS555.verifyDeathAtRecent30Days(examples.examplePersonRecentDeath))
+
+    def test_PersonNotRecentDeath(self):
+        self.assertFalse(CS555.verifyDeathAtRecent30Days(examples.examplePersonNotRecentDeath))
+
 if __name__ == '__main__':
     project03.processFile(project03.GEDCOM_FILE)
     unittest.main()
