45c49a6b714fa31d608339738e1a4ba2a7c8810e
achen3680 <44931055+achen3680@users.noreply.github.com>
2020-10-05 13:52:09 -0400

    remove married and unmarried examples

--- examples.py
+++ examples.py
@@ -149,26 +149,3 @@ exampleImproperFamilyWithWidow = {
     'children': ['@I1@', '@I6@', '@I8@', '@I10@']
 }
 
-examplePersonMarried = {
-    'id': '@I1@',
-    'name': 'Alice /Trout/',
-    'gender': 'F',
-    'birthday': '2 DEC 1970',
-    'age': 49,
-    'alive': True,
-    'death': 'NA',
-    'child': '@F2@',
-    'spouse': '@F1@'
-}
-
-examplePersonNonMarried = {
-    'id': '@I10@',
-    'name': 'Bret /Trout/',
-    'gender': 'M',
-    'birthday': '28 JUL 1971',
-    'age': 49,
-    'alive': False,
-    'death': 'NA',
-    'child': '@F2@',
-    'spouse': 'NA'
-}
